758f5563e7fa8e42f6a9e686260c100cedbc45d7
Felipe Fernandes <hey_felipao@gmail.com>
2020-08-27 08:56:33 -0300

    first

---
+++ Logger.js
@@ -0,0 +1,66 @@
+const fs = require('fs');
+const moment = require( 'moment' );
+const path = require('path');
+
+let dir = path.join(__dirname, '/logs'); console.log(dir);
+
+if (!fs.existsSync(dir)){
+    try {
+        fs.mkdirSync(dir);
+    } catch ( error ) {
+        console.log('Erro ao criar diretorio para Logs!');
+    }
+}
+
+class Logger {
+    info(msg) {
+        const now = moment().utcOffset("-03:00").format( 'YYYY-MM-DD' );
+        
+        fs.appendFile(`${dir}info-` + now + '.log', "INFO: " + msg + '\n', (err) => {
+            if (err) {
+                console.log('Erro ao gravar log logs/server.log', err);
+            }
+        });
+    }
+
+    debug(url, request, response, route) {
+        const requestBody = JSON.parse(JSON.stringify(request))
+        const responseBody = JSON.parse(JSON.stringify(response))
+
+        if (requestBody.password) {
+            delete requestBody.password;
+            
+            requestBody.password = "ENCRYPTED";
+        }
+
+        if (responseBody.cpf) {
+            responseBody.cpf = "*****" + responseBody.cpf.slice(5);
+        }
+
+        if (Object.values(requestBody)[0].cpfCliente) {
+            let cpf = Object.values(requestBody)[0].cpfCliente;
+            let newCpf = "*****" +  cpf.substring(5)
+
+            Object.values(requestBody)[0].cpfCliente = newCpf
+        }
+             
+        if (requestBody.sessionToken) {
+            requestBody.sessionToken = "**************" + requestBody.sessionToken.slice(15);
+        }
+
+        const now = moment().utcOffset("-03:00").format('YYYY-MM-DD');
+        const nowWithMinutes = moment().utcOffset("-03:00").format('YYYY-MM-DD HH:mm:ss');
+
+        
+        fs.appendFile(`${dir}routes-` + now + '.log',
+         "ROUTE: " + route + '\n' + 'DATE: ' + nowWithMinutes + '\n' + 'URL: ' + url + '\n' + 'REQUEST: ' + JSON.stringify(requestBody) + '\n' + 'RESPONSE: ' + JSON.stringify(responseBody)  + '\n' + '----' + '\n', 
+         (err) => {           
+            if (err) {
+                console.log('Erro ao gravar log logs/server.log');
+            }
+        });
+    }
+}
+
+module.exports = Logger;
+*/
---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    return res.status(200).send({
+        message: 'esta funciondo!'
+    })
+})
+
+app.get('/teste', async (req, res) => {
+    try {
+        axios.interceptors.request.use(req => {
+            console.log(`${req.method} ${req.url}`);
+            // Important: request interceptors **must** return the request.
+            return req;
+        });
+
+        axios.interceptors.response.use(res => {
+            console.log(res.data);
+            // Important: response interceptors **must** return the response.
+            return res;
+        });
+        
+        const resposta = await axios.get('https://swapi.dev/api/people/1/');
+
+        return res.status(200).send(resposta.data);
+    } catch (error) {
+        return res.status(500).send({
+            message: error
+        })
+    }
+})
+
+app.listen(PORT, () => console.log(`Backend is up on ${PORT}`));
